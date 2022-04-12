b1f607ca36e9287059653bf96593f7c5f48c20d1
Phyl12 <shamisomavedzenge@gmail.com>
2022-04-12 15:48:44 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Printing alphabets in lower case
+ */
+void print_alphabet(void)
+{
+char d;
+for (d= 'a'; d <= 'z'; d++)
+_putchar (d);
+_putchar ('\n');
+}
+   
